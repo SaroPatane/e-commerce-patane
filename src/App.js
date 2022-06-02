c1a91117603d5ee24b2952126032fa44e5ae88e3
Saro <saropatane2@gmail.com>
2022-06-01 21:41:51 -0300

    tercer desafio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Imagen from './imagenes/Imagen';
+
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+        <Imagen />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/imagenes/Imagen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Imagen() {
+  return (
+    <img src="https://us.123rf.com/450wm/dzein/dzein1502/dzein150200002/36125549-construcci%C3%B3n-registrarte-fondo-blanco-ilustraci%C3%B3n-vectorial-3d-mesh.jpg?ver=6" />
+  )
+}
+
+export default Imagen
